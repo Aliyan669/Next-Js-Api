0c5df0a71017135230ffdedb84b50e9328db1ad7
Aliyan Amir <tboy02722@gmail.com>
2023-11-17 02:44:40 +0500

    Next-Js Practice

---
+++ src/lib/db.js
@@ -0,0 +1,4 @@
+const { username, password } = process.env;
+
+// export const connectionSrt= "mongodb+srv://"+username+":"+password+"@cluster0.yxcgjp2.mongodb.net/productDB?retryWrites=true&w=majority"
+export const connectionSrt = "mongodb+srv://tboy02722:aliyan12345@cluster0.yxcgjp2.mongodb.net/productDB?retryWrites=true&w=majority"
