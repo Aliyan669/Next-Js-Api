098bd192f33ed08059026329eace36b4afa1353c
Aliyan Amir <tboy02722@gmail.com>
2023-11-19 23:30:43 +0500

    Next-Js Api

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,10 +1,29 @@
 import { connectionSrt } from "@/lib/db";
+import { Product } from "@/lib/model/product";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET() {
 
+    // await mongoose.connect(connectionSrt);
+    // const data = await Product.find()
+    let data = []
+    try {
+        await mongoose.connect(connectionSrt);
+        data = await Product.find()
+
+    } catch (error) {
+        data = { success: false }
+    }
+    return NextResponse.json({ result: data, success: true })
+
+}
+
+export async function POST(request) {
+    const payload = await request.json();
     await mongoose.connect(connectionSrt);
-    return NextResponse.json({ result: true })
+    let product = new Product(payload);
+    const result = await product.save()
+    return NextResponse.json({ result, success: true })
 
 }
