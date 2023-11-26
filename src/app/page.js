112131b48bd151cfbee9d274d5fb78464043a78f
Aliyan Amir <tboy02722@gmail.com>
2023-11-27 03:11:08 +0500

    Next-Js Api

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1>Hello Next Js Api</h1>
+      <h1>Hello-Next Js Api</h1>
       <Link href={"/product"}>Product</Link><br />
       <Link href={"/addProduct"}>Add Product</Link>
     </div>
